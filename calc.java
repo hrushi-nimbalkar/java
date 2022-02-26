d7c66630a606ac32f120fe8f7987ad8154857b83
hrushikesh <hrushikeshnimbalkarhmn1@gmail.com>
2022-02-26 10:31:43 +0530

    hello

---
+++ calc.java
@@ -0,0 +1,11 @@
+public class calc
+{
+    public static void main(String s[])
+    {
+        int a,b;
+        a=10;
+        b=20;
+        int c =a+b;
+        System.out.println("the addition is"+c);
+    }
+}
